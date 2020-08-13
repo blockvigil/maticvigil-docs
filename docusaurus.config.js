c644a8226ab7874f8dfad7f30b952f7d7f61238b
Swaroop Hegde <email@swaroophegde.com>
2020-08-13 21:20:07 +0530

    Updated GA property

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,8 @@ module.exports = {
       [
           '@docusaurus/plugin-google-analytics',
           {
-              id: 'analytics'
+              id: 'analytics',
+              trackingID: 'UA-113891968-6'
           }
       ],
       [
